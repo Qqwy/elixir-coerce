9026159e8238d13b60c0ecca5cac7ecc8c110a78
Qqwy/Wiebe-Marten Wijnja <qqwy@gmx.com>
2017-07-18 23:35:28 +0200

    Fixes warnings.

--- lib/coerce.ex
+++ lib/coerce.ex
@@ -23,19 +23,27 @@ defmodule Coerce do
     Module.concat([Coerce.Implementations, a_mod, b_mod]).coerce(a, b)
   end
 
+  for {guard, mod} <- @builtin_guards_list do
+    def coerce(a = %a_struct_mod{}, b) when unquote(guard)(b) do
+      Module.concat([Coerce.Implementations, a_struct_mod, unquote(mod)]).coerce(a, b)
+    end
+
+    def coerce(a, b = %b_struct_mod{}) when unquote(guard)(a) do
+      Module.concat([Coerce.Implementations, unquote(mod), b_struct_mod]).coerce(a, b)
+    end
+  end
+
   for {guard_a, a_mod} <- @builtin_guards_list, {guard_b, b_mod} <- @builtin_guards_list do
     if guard_a == guard_b do
       def coerce(a, b) when unquote(guard_a)(a) and unquote(guard_a)(b) do
         {a, b}
       end
+
     else
       primary_module = Module.concat([Coerce.Implementations, a_mod, b_mod])
-      IO.puts(primary_module)
-      if Code.ensure_compiled?(primary_module) do
-        IO.puts("TEST")
-        def coerce(a, b) when unquote(guard_a)(a) and unquote(guard_b)(b) do
-          unquote(primary_module).coerce(a, b)
-        end
+      def coerce(a, b) when unquote(guard_a)(a) and unquote(guard_b)(b) do
+        # Uses Kernel.apply to mitigate warning if module does not exist...
+        apply(unquote(primary_module), :coerce, [a, b])
       end
     end
   end
