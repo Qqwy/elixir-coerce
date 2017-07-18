342b698ae44ed70f9703e4baf32de41775ba5d5e
Qqwy/Wiebe-Marten Wijnja <qqwy@gmx.com>
2017-07-18 23:27:54 +0200

    nearly working.

--- lib/coerce.ex
+++ lib/coerce.ex
@@ -4,65 +4,46 @@ defmodule Coerce do
   """
 
   @builtin_guards_list [
-    {:is_success_tuple,  FunLand.Builtin.SuccessTuple},
-    is_tuple: FunLand.Builtin.Tuple,
-    is_atom: FunLand.Builtin.Atom,
-    is_list: FunLand.Builtin.List,
-    is_map: FunLand.Builtin.Map,
-    is_bitstring: FunLand.Builtin.BitString,
-    is_integer: FunLand.Builtin.Integer,
-    is_float: FunLand.Builtin.Float,
-    is_function: FunLand.Builtin.Function,
-    is_pid: FunLand.Builtin.PID,
-    is_port: FunLand.Builtin.Port,
-    is_reference: FunLand.Builtin.Reference]
+    is_tuple: Tuple,
+    is_atom: Atom,
+    is_list: List,
+    is_map: Map,
+    is_bitstring: BitString,
+    is_integer: Integer,
+    is_float: Float,
+    is_function: Function,
+    is_pid: PID,
+    is_port: Port,
+    is_reference: Reference]
 
   def coerce(a = %a_mod{}, b = %a_mod{}) do
-    {a, a}
+    {a, b}
   end
   def coerce(a = %a_mod{}, b = %b_mod{}) do
-    # :"Elixir.Coerce.Implementations.#{a_mod}.#{b_mod}".coerce(a, b)
     Module.concat([Coerce.Implementations, a_mod, b_mod]).coerce(a, b)
   end
 
   for {guard_a, a_mod} <- @builtin_guards_list, {guard_b, b_mod} <- @builtin_guards_list do
     if guard_a == guard_b do
-      def coerce(a, b) when guard_a(a) and guard_a(b) do
+      def coerce(a, b) when unquote(guard_a)(a) and unquote(guard_a)(b) do
         {a, b}
       end
     else
-      def coerce(a, b) when guard_a(a) and guard_b(b) do
-        Module.concat([Coerce.Implementations, a_mod, b_mod]).coerce(a, b)
-      end
-    end
-  end
-
-
-  defmacro defcoercion(first_module, second_module, [do: block]) do
-    # primary_module = :"Elixir.Coerce.Implementations.#{first_module}.#{second_module}"
-    # secondary_module = :"Elixir.Coerce.Implementations.#{second_module}.#{first_module}"
-    primary_module = Module.concat(Coerce.Implementations, first_module, second_module)
-    secundary_module = Module.concat(Coerce.Implementations, second_module, first_module)
-    quote bind_quoted: [primary_module: primary_module, secondary_module: secondary_module] do
-      defmodule primary_module do
-        @behaviour Coerce.Behaviour
-        unquote(block)
-      end
-      unless Module.defines?(primary_module, :coerce, 2) do
-        raise "Error: `Coerce.defcoercion` implementation does not implement `coerce/2`."
-      end
-
-      defmodule secondary_module do
-        def coerce(lhs, rhs) do
-          {rhs, lhs} = primary_module.coerce(rhs, lhs)
-          {lhs, rhs}
+      primary_module = Module.concat([Coerce.Implementations, a_mod, b_mod])
+      IO.puts(primary_module)
+      if Code.ensure_compiled?(primary_module) do
+        IO.puts("TEST")
+        def coerce(a, b) when unquote(guard_a)(a) and unquote(guard_b)(b) do
+          unquote(primary_module).coerce(a, b)
         end
       end
     end
   end
-  defmacro defcoercion(_, _, _), do: raise "Error: `Coerce.defcoercion` called with improper parameters."
 
   defmodule Behaviour do
     @callback coerce(a, b) :: {a, a} when a: any, b: any
+  end
+
+  defmodule Implementations do
   end
 end
